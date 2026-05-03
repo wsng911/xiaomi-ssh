050e71b07eb454d335cb1da65fc22824164c32ba
wsng911 <824559662@qq.com>
2026-05-03 18:08:15 +0800

    add run.py entry point

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from menu import menu
+
+if __name__ == '__main__':
+    menu()
